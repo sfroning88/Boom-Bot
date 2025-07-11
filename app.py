2c5f33c51c441902ef290378c1327ea1bd421637
Sean Froning <123010146+sfroning88@users.noreply.github.com>
2025-07-11 08:39:23 -0500

    Update app.py
    Minor spacing updates.

--- app.py
+++ app.py
@@ -5,11 +5,11 @@ from flask import Flask, render_template, request, jsonify
 from transformers import AutoModelForCausalLM, AutoTokenizer
 from transformers.pipelines import pipeline
 import torch
+
 from werkzeug.utils import secure_filename
 import tempfile
 
 ALLOWED_EXTENSIONS = {'csv', 'xls', 'xlsx'}
-
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
